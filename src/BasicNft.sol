65681bb5ca56b09ec2f8b47e841997b24014a744
q10357 <isabel@chaparian.no>
2024-12-01 11:37:56 +0100

    don't include libs please...

--- src/BasicNft.sol
+++ src/BasicNft.sol
@@ -4,12 +4,19 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 pragma solidity ^0.8.18;
 
-contract BasicNft is ERC721 {
+contract BasicNFT is ERC721 {
     uint256 private s_tokenCounter;
+    mapping(uint256 => string) private s_tokenIdToUri;
 
-    constructor() ERC721("Doggie", "DOG") {
+    constructor() ERC721("BasicNFT", "BFT") {
         s_tokenCounter = 0;
     }
 
-    function tokenURI(uint256 tokenId) public view override returns (string memory) {}
+    function mintNFT(string memory tokenUri) public returns (uint256) {
+        s_tokenIdToUri[s_tokenCounter] = tokenUri;
+    }
+
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        return s_tokenIdToUri[tokenId];
+    }
 }
