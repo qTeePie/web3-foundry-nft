6a814f9d1f83673c34e3b66ab6a2cf8eb4d92e4a
q10357 <isabel@chaparian.no>
2024-12-01 03:57:30 +0100

    fmt

--- src/BasicNft.sol
+++ src/BasicNft.sol
@@ -5,6 +5,11 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 pragma solidity ^0.8.18;
 
 contract BasicNft is ERC721 {
+    uint256 private s_tokenCounter;
 
-    constructor() ERC721("Doggie", "DOG"){}
+    constructor() ERC721("Doggie", "DOG") {
+        s_tokenCounter = 0;
+    }
+
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {}
 }
