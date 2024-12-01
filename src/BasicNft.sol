9b4d55b6429fb50c2a2634d8d13186b527607257
q10357 <isabel@chaparian.no>
2024-12-01 03:49:47 +0100

    cleanup

--- src/BasicNft.sol
+++ src/BasicNft.sol
@@ -4,4 +4,7 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 pragma solidity ^0.8.18;
 
-contract BasicNft is ERC721 {}
+contract BasicNft is ERC721 {
+
+    constructor() ERC721("Doggie", "DOG"){}
+}
