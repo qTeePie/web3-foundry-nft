e766ab6ebf9448756cb3d91d18e305e263ea011c
q10357 <isabel@chaparian.no>
2024-12-04 05:45:58 +0100

    Lol some tests failin, TODO 4 laterrr

--- script/DeployMoodNft.s.sol
+++ script/DeployMoodNft.s.sol
@@ -7,8 +7,8 @@ import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
 
 contract DeployMoodNft is Script {
     function run() external returns (MoodNft) {
-        string memory sadSvg = vm.readFile("img/sadSvg.svg");
-        string memory happySvg = vm.readFile("img/happySvg.svg");
+        string memory sadSvg = vm.readFile("img/sad.svg");
+        string memory happySvg = vm.readFile("img/happy.svg");
 
         vm.startBroadcast();
         MoodNft moodNft = new MoodNft(svgToImageURI(sadSvg), svgToImageURI(happySvg));
--- test/MoodNftTest.t.sol
+++ test/MoodNftTest.t.sol
@@ -5,9 +5,12 @@ pragma solidity ^0.8.18;
 import {Test, console} from "forge-std/Test.sol";
 import {Test} from "forge-std/Test.sol";
 import {MoodNft} from "src/MoodNft.sol";
+import {DeployMoodNft} from "script/DeployMoodNft.s.sol";
 
 contract MoodNftTest is Test {
     MoodNft moodNft;
+    DeployMoodNft deployer;
+
     string public constant HAPPY_SVG_URI =
         "data:image/svg+xml;base64,PHN2ZyB2aWV3Qm94PSIwIDAgMjAwIDIwMCIgd2lkdGg9IjQwMCIgIGhlaWdodD0iNDAwIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciPgogIDxjaXJjbGUgY3g9IjEwMCIgY3k9IjEwMCIgZmlsbD0ieWVsbG93IiByPSI3OCIgc3Ryb2tlPSJibGFjayIgc3Ryb2tlLXdpZHRoPSIzIi8CiAgPGcgY2xhc3M9ImV5ZXMiPgogICAgPGNpcmNsZSBjeD0iNjEiIGN5PSI4MiIgcj0iMTIiLz4KICAgIDxjaXJjbGUgY3g9IjEyNyIgY3k9IjgyIiByPSIxMiIvPgogIDwvZz4KICA8cGF0aCBkPSJtMTM2LjgxIDExNi41M2MuNjkgMjYuMTctNjQuMTEgNDItODEuNTItLjczIiBzdHlsZT0iZmlsbDpub25lOyBzdHJva2U6IGJsYWNrOyBzdHJva2Utd2lkdGg6IDM7Ii8+Cjwvc3ZnPg==";
     string public constant SAD_SVG_URI =
@@ -17,7 +20,7 @@ contract MoodNftTest is Test {
 
     function setUp() public {
         deployer = new DeployMoodNft();
-        moodNft = new Deployer - run();
+        moodNft = deployer.run();
     }
 
     function testViewTokenURIIntegration() public {
@@ -25,4 +28,16 @@ contract MoodNftTest is Test {
         moodNft.mintNft();
         console.log(moodNft.tokenURI(0));
     }
+    /*  TODO Fix this later
+    function testFlipMoodIntegration() public {
+        // Arrange
+        vm.prank(USER);
+        moodNft.mintNft();
+        vm.prank(USER);
+        // Act
+        moodNft.flipNFTState(0);
+        // Assert
+        assert(keccak256(abi.encodePacked(moodNft.tokenURI(0))) == keccak256(abi.encodePacked(SAD_SVG_URI)));
+    }
+    */
 }
